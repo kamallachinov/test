73854dc50adcde943fad1d31d1a8ac731d5740ea
kamallachinov <kamallachinov5@gmail.com>
2024-03-18 16:58:59 +0400

    Salamaleykum

---
+++ main.js
@@ -0,0 +1,53 @@
+// function formatName(myAge, myName) {
+//     var uni = "Baku state university";
+//     var myCar = "Hyundai"
+
+//     // console.log(surname, 'surname')
+
+//     console.log(this)
+
+// }
+// formatName()
+
+// formatName(20, "Kamal");
+
+
+// function runFunction(name) {
+//     console.log(`${name} is running now!`)
+
+// }
+
+
+// const obj = {
+//     name: "Kamal",
+//     surname: "Lachinov",
+//     age: 20,
+//     runFunction: console.log(this)
+// }
+// // obj.runFunction;
+// // obj['runFunction'];
+// console.log(obj)
+
+
+const updateRepo = "Kamal"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
